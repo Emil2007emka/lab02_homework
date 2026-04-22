3e2e140200a6f2571e18798ecc1630ea760e7a14
Emil2007emka <emilrzaev28@gmail.com>
2026-04-22 23:59:35 +0300

    added comments to code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,14 @@
+// Lab02 Homework Part II
+// This program asks for user's name and greets them
+
 #include <iostream>
 #include <string>
 
 int main()
 {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::string name;  // variable to store user's name
+    std::cout << "Enter your name: ";  // prompt for input
+    std::cin >> name;  // read user's name
+    std::cout << "Hello world from " << name << std::endl;  // output greeting
     return 0;
 }
